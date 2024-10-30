58f0d388917402fd383d9b5b679741f70751ef3b
Dasun Piyumal <w.piyumal2319@gmail.com>
2024-10-30 15:07:53 +0530

    Refactor API base URL to use dynamic host and protocol

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
-const API_BASE_URL = 'http://localhost:3000/dashboard';
+// const API_BASE_URL = 'http://localhost:3000/dashboard';
 // const API_BASE_URL = 'http://192.168.109.29:3000/dashboard';
+const API_BASE_URL = `${window.location.protocol}//${window.location.hostname}:3000/dashboard`;
+
 
 
 const api = axios.create({ 
